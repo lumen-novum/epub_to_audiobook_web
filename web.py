3f97adaa55adf27fec0bb964d27a78af1cfe5ca5
lumen-novum <novablackxd@gmail.com>
2024-09-11 22:13:25 -0500

    Create instance of Flask class

--- web.py
+++ web.py
@@ -1,5 +1,7 @@
-from flask import request, render_template
+from flask import request, render_template, Flask
 from main import main
+
+app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
